b272ef8bb0c7c9410793f045792cbb743fe33535
HIMOTHY-HIMMONS <vijval.yaramati.310@k12.friscoisd.org>
2024-08-08 17:22:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function SwitchOn() {
+    document.getElementById("bulb").src = "./pic_bulbon.gif"
+    document.getElementById
+}
+
+function SwitchOff() {
+    document.getElementById("bulb").src = "./pic_bulboff.gif"
+}
